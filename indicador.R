b4fcdbab7df6401e5baf21869a2c02585b1ea72b
Bastián Olea Herera <bastianolea@gmail.com>
2025-01-31 17:04:15 -0300

    correcciones

---
+++ indicador.R
@@ -0,0 +1,10 @@
+library(readr)
+
+# cargar datos
+sicvir <- read_csv2("datos/sicvir_indicadores_rurales_2025.csv")
+etiquetas <- read_csv2("datos/sicvir_etiquetas_2025.csv")
+
+
+# guardar datos
+write_csv2(sicvir, "/Users/baolea/R/subdere/indicadores/sicvir_indicadores_rurales.csv")
+write_csv2(etiquetas, "/Users/baolea/R/subdere/indicadores/sicvir_etiquetas.csv")
